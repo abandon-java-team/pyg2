3f14addf09a28817869ceb14164b590a56052842
bblayers <493060643@qq.com>
2019-05-18 20:20:10 +0800

    我的关注 我的收藏

--- pinyougou_parent/pinyougou_cart_web/src/main/webapp/js/service/cartService.js
+++ pinyougou_parent/pinyougou_cart_web/src/main/webapp/js/service/cartService.js
@@ -3,20 +3,36 @@ app.service('cartService', function ($http) {
     //购物车列表
     this.findCartList = function () {
         return $http.get('cart/findCartList.do');
-    }
+    };
 
     //添加商品到购物车
     this.addGoodsToCartList = function (itemId, num) {
         return $http.get('cart/addGoodsToCartList.do?itemId=' + itemId + '&num=' + num);
-    }
+    };
+
+    //从收藏夹添加商品到购物车
+    this.addGoodsToCartListFromCollect = function (itemId) {
+        return $http.get('collect/addGoodsToCartListFromCollect.do?itemId=' + itemId)
+    };
 
     //获取地址列表
     this.findAddressList = function () {
         return $http.get('address/findListByLoginUser.do');
-    }
+    };
 
     this.submitOrder = function (order) {
         return $http.post("order/add.do", order);
-    }
+    };
 
+    this.findCollect = function () {
+        return $http.get('collect/findCollect.do');
+    };
+
+    this.addGoodsToCollect = function (itemId) {
+        return $http.get('collect/addGoodsToCollect.do?itemId='+itemId);
+    };
+
+    this.findByItemId = function (itemId) {
+        return $http.get('collect/findByItemId.do?itemId=' + itemId);
+    }
 });
